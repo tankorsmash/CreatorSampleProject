af0604bf84ecc32fc8df4f7c0fcd522b82b0d330
tankorsmash <tankorsmash@gmail.com>
2018-02-12 14:40:09 -0500

    first commit of SampleProject for testing layout support
    thought this had already had a repo, but I guess not

---
+++ packages/creator-luacpp-support/core/parser/Layout.js
@@ -0,0 +1,23 @@
+const Node = require('./Node');
+const Utils = require('./Utils');
+const state = require('./Global').state;
+
+class Layout extends Node {
+    constructor(data) {
+        super(data);
+        this._jsonNode.object_type = 'Layout';
+    }
+
+    parse_properties() {
+        super.parse_properties();
+        this._properties = {node: this._properties};
+
+        let spr_component = Node.get_node_component_of_type(this._node_data, 'cc.Sprite');
+
+        //TODO use these components in this._properties ie vertical layout
+        let lay_component = Node.get_node_component_of_type(this._node_data, 'cc.Layout');
+
+    }
+}
+
+module.exports = Layout;
